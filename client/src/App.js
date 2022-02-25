c392b98dc633d8ca6c6e3f9d5e95d3373549276c
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-02-24 21:18:41 -0500

    Initialize React app

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Bug Off</p>
+        <a
+          className="App-link"
+          href="https://github.com/rakilahmed/Bug-Off/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub...
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
