87af3fb4a26d51d1349fb632d1549844ba6ae252
Syed Hussain <smth7700@gmail.com>
2022-03-08 16:36:05 -0500

    Update Login Register Pgs

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Typography, TextField, Button, Grid, Link } from "@mui/material";
+import { Box, TextField, Button, Container, Grid, Link } from "@mui/material";
 import { LoginSharp } from "@mui/icons-material";
+import logo from "../assets/logo.png";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -11,57 +11,59 @@ const Login = () => {
   };
 
   return (
-    <Box
-      sx={{
-        marginTop: 30,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Typography variant="h4">Welcome to Bug Off</Typography>
-      <Box sx={{ mt: 1 }}>
-        <TextField
-          fullWidth
-          required
-          margin="normal"
-          id="email"
-          name="email"
-          label="Email Address"
-        />
-        <TextField
-          fullWidth
-          required
-          margin="normal"
-          type="password"
-          id="password"
-          name="password"
-          label="Password"
-        />
-        <Button
-          fullWidth
-          variant="contained"
-          color="success"
-          sx={{ mt: 1, mb: 2 }}
-          startIcon={<LoginSharp />}
-          onClick={handleLogin}
-        >
-          LOGIN
-        </Button>
-        <Grid container>
-          <Grid item xs>
-            <Link href="#" varient="body2">
-              Forgot password?
-            </Link>
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 20,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <img src={logo} alt="Bug Off logo" />
+        <Box sx={{ mt: 2 }}>
+          <TextField
+            fullWidth
+            required
+            margin="normal"
+            id="email"
+            name="email"
+            label="Email Address"
+          />
+          <TextField
+            fullWidth
+            required
+            margin="normal"
+            type="password"
+            id="password"
+            name="password"
+            label="Password"
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            color="success"
+            sx={{ mt: 1, mb: 2 }}
+            startIcon={<LoginSharp />}
+            onClick={handleLogin}
+          >
+            LOGIN
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="/login" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="/register" variant="body2">
+                Don't have an account? Register!
+              </Link>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Link href="/register" variant="body2">
-              Don't have an account? Register
-            </Link>
-          </Grid>
-        </Grid>
+        </Box>
       </Box>
-    </Box>
+    </Container>
   );
 };
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,67 +1,81 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Box, Typography, TextField, Button, Grid, Link } from "@mui/material";
+import { Box, TextField, Button, Container, Grid, Link } from "@mui/material";
 import { HowToRegSharp } from "@mui/icons-material";
+import logo from "../assets/logo.png";
 
 const Register = () => {
   const navigate = useNavigate();
 
   const handleRegister = () => {
-    navigate("/login");
+    navigate("/");
   };
 
   return (
-    <Box
-      sx={{
-        marginTop: 30,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Typography variant="h4">Welcome to Bug Off</Typography>
-      <Box sx={{ mt: 1 }}>
-        <Grid container spacing={2}>
-          <Grid item xs={12}>
-            <TextField fullWidth required id="name" name="name" label="Name" />
+    <Container maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 20,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <img src={logo} alt="Bug Off logo" />
+        <Box sx={{ mt: 2 }}>
+          <TextField
+            fullWidth
+            required
+            margin="normal"
+            id="name"
+            name="name"
+            label="Name"
+          />
+          <TextField
+            fullWidth
+            required
+            margin="normal"
+            id="email"
+            name="email"
+            label="Email Address"
+          />
+          <TextField
+            fullWidth
+            required
+            margin="normal"
+            type="password"
+            id="password"
+            name="password"
+            label="Password"
+          />
+          <TextField
+            fullWidth
+            required
+            margin="normal"
+            type="password"
+            id="confirm-password"
+            name="confirm-password"
+            label="Confirm Password"
+          />
+          <Button
+            fullWidth
+            variant="contained"
+            color="success"
+            sx={{ mt: 1, mb: 2 }}
+            startIcon={<HowToRegSharp />}
+            onClick={handleRegister}
+          >
+            REGISTER
+          </Button>
+          <Grid container justifyContent="flex-end">
+            <Grid item>
+              <Link href="/login" variant="body2">
+                Already have an account? Login!
+              </Link>
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              required
-              id="email"
-              name="email"
-              label="Email Address"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField
-              fullWidth
-              required
-              type="password"
-              id="password"
-              name="password"
-              label="Password"
-            />
-          </Grid>
-        </Grid>
-        <Button
-          fullWidth
-          variant="contained"
-          color="success"
-          sx={{ mt: 1, mb: 2 }}
-          startIcon={<HowToRegSharp />}
-          onClick={handleRegister}
-        >
-          Register
-        </Button>
-        <Grid item>
-          <Link href="/login" variant="body2">
-            Already have an account? Login
-          </Link>
-        </Grid>
+        </Box>
       </Box>
-    </Box>
+    </Container>
   );
 };
 
