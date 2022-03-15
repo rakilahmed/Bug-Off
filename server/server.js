37d0144d9c2949473c7100a51a6f6760782ba074
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-03-15 03:00:55 -0400

    CORS added to access API from frontend

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config({ path: './config/.env' });
 const express = require('express');
+const cors = require('cors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const ticketRoutes = require('./routes/ticketRoutes');
@@ -8,6 +9,7 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('tiny'));
