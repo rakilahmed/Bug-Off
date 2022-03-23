faa24606b8d460a4a3502cb202fe6831b5997ddb
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-03-22 22:51:57 -0400

    Header added to the Profile component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -37,7 +37,7 @@ const Profile = () => {
 
     Promise.all(promises)
       .then(() => {
-        setMessage('Profile Updated');
+        setMessage('Profile updated');
         setName('');
         setEmail('');
         setPassword('');
@@ -57,7 +57,7 @@ const Profile = () => {
       container
       alignItems="center"
       justifyContent="center"
-      style={{ minHeight: '100vh' }}
+      style={{ minHeight: '90vh' }}
     >
       <Paper
         sx={{
