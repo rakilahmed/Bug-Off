171aa9f17b20eafb1a2aed3fa18883a81d2577c1
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-04-09 03:24:04 -0400

    Number of tasks added

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -5,14 +5,12 @@ import { useEffect, useState } from 'react';
 import { useAuth } from '../../firebase/AuthContext';
 
 const TICKETS = 'https://bugoff.rakilahmed.com/api/tickets';
+const TASKS = 'https://bugoff.rakilahmed.com/api/tasks';
 
-const Overview = ({
-  overdueTickets = 2,
-  closedTickets = 7,
-  openTasks = 10,
-}) => {
+const Overview = ({ overdueTickets = 2, closedTickets = 7 }) => {
   const { getToken } = useAuth();
   const [ticketsCount, setTicketsCount] = useState(0);
+  const [tasksCount, setTasksCount] = useState(0);
   const formatValue = (value) => `${Number(value).toFixed(0)}`;
 
   axios.interceptors.request.use(
@@ -33,16 +31,18 @@ const Overview = ({
       );
     };
 
+    const fetchTasks = async () => {
+      const res = await axios.get(TASKS);
+      setTasksCount(res.data[0].tasks.length ? res.data[0].tasks.length : 0);
+    };
+
     fetchTickets();
+    fetchTasks();
   }, []);
 
   return (
     <>
-      <Grid
-        container
-        spacing={{ md: 3, sm: 2, xs: 3 }}
-        columns={{ xs: 4, sm: 8, md: 8 }}
-      >
+      <Grid container spacing={2} columns={{ xs: 4, sm: 8 }}>
         <Grid item xs={4}>
           <Paper
             sx={{
@@ -54,10 +54,10 @@ const Overview = ({
               boxShadow: 'rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;',
             }}
           >
-            <Typography variant="subtitle1" fontSize={20}>
+            <Typography variant="subtitle1" fontSize={20} noWrap>
               Open Tickets
             </Typography>
-            <Typography variant="subtitle2" fontSize={35}>
+            <Typography variant="subtitle2" fontSize={35} noWrap>
               <AnimatedNumber
                 value={ticketsCount}
                 formatValue={formatValue}
@@ -77,7 +77,7 @@ const Overview = ({
               boxShadow: 'rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;',
             }}
           >
-            <Typography variant="subtitle1" fontSize={20}>
+            <Typography variant="subtitle1" fontSize={20} noWrap>
               Overdue Tickets
             </Typography>
             <Typography variant="subtitle2" fontSize={35}>
@@ -103,7 +103,7 @@ const Overview = ({
             <Typography variant="subtitle1" fontSize={20}>
               Closed Tickets
             </Typography>
-            <Typography variant="subtitle2" fontSize={35}>
+            <Typography variant="subtitle2" fontSize={35} noWrap>
               <AnimatedNumber
                 value={closedTickets}
                 formatValue={formatValue}
@@ -123,12 +123,12 @@ const Overview = ({
               boxShadow: 'rgba(0, 0, 0, 0.45) 0px 25px 20px -20px;',
             }}
           >
-            <Typography variant="subtitle1" fontSize={20}>
-              Open Tasks
+            <Typography variant="subtitle1" fontSize={20} noWrap>
+              Tasks To Do
             </Typography>
-            <Typography variant="subtitle2" fontSize={35}>
+            <Typography variant="subtitle2" fontSize={35} noWrap>
               <AnimatedNumber
-                value={openTasks}
+                value={tasksCount}
                 formatValue={formatValue}
                 duration={500}
               />
