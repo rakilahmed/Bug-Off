6dbf9baf250c8236a00c77962f3378f920242157
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-04-19 05:47:48 -0400

    Task DB timestamp error fixed

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -17,7 +17,7 @@ const taskSchema = mongoose.Schema(
     ],
   },
   {
-    timestamps: true,
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' },
     versionKey: false,
   }
 );
