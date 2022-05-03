6aa8d68b5ef11451c9b1719fe0942befc13d334b
Rakil Ahmed <rakil.ahmed@outlook.com>
2022-05-03 00:09:48 -0400

    Limited closed tickets shown to user logic added

--- client/src/components/Tickets/ClosedTickets/ClosedTickets.jsx
+++ client/src/components/Tickets/ClosedTickets/ClosedTickets.jsx
@@ -1,9 +1,13 @@
-import { Paper, Typography } from '@mui/material/';
+import { useState } from 'react';
+import { Paper, Typography, Box } from '@mui/material/';
+import { BsArrowDown } from 'react-icons/bs';
 import ClosedTicket from './ClosedTicket';
 import { useTicketContext } from '../TicketProvider';
 
 const ClosedTickets = () => {
   const { closedTickets } = useTicketContext();
+  const [loadMore, setLoadMore] = useState(false);
+
   return (
     <Paper
       sx={{
@@ -13,14 +17,48 @@ const ClosedTickets = () => {
       }}
     >
       <Typography variant="h6">Closed Tickets</Typography>
-      {closedTickets.length > 0 ? (
+      {!loadMore && closedTickets.length > 0 ? (
         closedTickets.slice(0, 5).map((ticket) => {
+          return <ClosedTicket key={ticket._id} ticket={ticket} />;
+        })
+      ) : loadMore ? (
+        closedTickets.slice(0, 10).map((ticket) => {
           return <ClosedTicket key={ticket._id} ticket={ticket} />;
         })
       ) : (
         <Typography sx={{ marginTop: 2 }} varient="body1">
           No closed tickets to show.
         </Typography>
+      )}
+
+      {!loadMore && closedTickets.length > 5 ? (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+          }}
+        >
+          <Typography
+            variant="body2"
+            color={'secondary'}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              cursor: 'pointer',
+            }}
+            onClick={() => setLoadMore(true)}
+          >
+            Load More <BsArrowDown style={{ marginLeft: 5 }} />
+          </Typography>
+        </Box>
+      ) : (
+        loadMore &&
+        closedTickets.length > 10 && (
+          <Typography variant="body2" color={'secondary'}>
+            You can only see the first 10 closed tickets here. <br /> Delete few
+            closed tickets from the list to see more.
+          </Typography>
+        )
       )}
     </Paper>
   );
